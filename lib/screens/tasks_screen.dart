272291cc66c56fb09dc9919f1ecdfadfd57d7e0e
AmmarSultanDev <ammarsultan1445@gmail.com>
2024-10-22 17:58:16 +0100

    chore: Update FloatingActionButton child to use const

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -13,7 +13,7 @@ class TasksScreen extends StatelessWidget {
       backgroundColor: Colors.lightBlueAccent,
       floatingActionButton: FloatingActionButton(
           backgroundColor: Colors.lightBlueAccent,
-          child: Icon(Icons.add),
+          child: const Icon(Icons.add),
           onPressed: () {
             showModalBottomSheet(
                 context: context,
@@ -22,31 +22,31 @@ class TasksScreen extends StatelessWidget {
                         child: Container(
                       padding: EdgeInsets.only(
                           bottom: MediaQuery.of(context).viewInsets.bottom),
-                      child: AddTaskScreen(),
+                      child: const AddTaskScreen(),
                     )));
           }),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           Container(
-            padding: EdgeInsets.only(
+            padding: const EdgeInsets.only(
                 top: 60.0, left: 30.0, right: 30.0, bottom: 30.0),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-                CircleAvatar(
+                const CircleAvatar(
+                  backgroundColor: Colors.white,
+                  radius: 30.0,
                   child: Icon(
                     Icons.list,
                     size: 30.0,
                     color: Colors.lightBlueAccent,
                   ),
-                  backgroundColor: Colors.white,
-                  radius: 30.0,
                 ),
-                SizedBox(
+                const SizedBox(
                   height: 10.0,
                 ),
-                Text(
+                const Text(
                   'Todoey',
                   style: TextStyle(
                     color: Colors.white,
@@ -56,7 +56,7 @@ class TasksScreen extends StatelessWidget {
                 ),
                 Text(
                   '${Provider.of<TaskData>(context).taskCount} Tasks',
-                  style: TextStyle(
+                  style: const TextStyle(
                     color: Colors.white,
                     fontSize: 18,
                   ),
@@ -66,15 +66,15 @@ class TasksScreen extends StatelessWidget {
           ),
           Expanded(
             child: Container(
-              padding: EdgeInsets.symmetric(horizontal: 20.0),
-              decoration: BoxDecoration(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              decoration: const BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.only(
                   topLeft: Radius.circular(20.0),
                   topRight: Radius.circular(20.0),
                 ),
               ),
-              child: TasksList(),
+              child: const TasksList(),
             ),
           ),
         ],
